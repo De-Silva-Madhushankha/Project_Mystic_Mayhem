40ca3fdc15617e88c80cfd504cc2f1680a722493
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2024-03-07 23:36:01 +0530

    Changed addHero, sellHero, buyHero, updateStatus Added new method called trainHero to create a Character object

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,34 @@ import Equipment.*;
 
 public class Main {
     public static void main(String[] args) {
-//        UserProfile user01 = new UserProfile("Razor","Razor_0.1");
-//        user01.setHomeground("Arcane");
-//
-//        Character shooter = new Shooter();
-//        Character ranger = new Ranger();
-//
-//        System.out.println(shooter.getClass().getSuperclass().getSimpleName());
-//
-//        Equipment fleece = new Fleece();
-//        System.out.println(fleece.getClass().getSuperclass().getInterfaces()[0].getSimpleName());
 
+        System.out.println("---------------------------------------------------------------------------------------");
+        System.out.println("|                              Mystic Mayhem                                          |");
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name =scan.nextLine();
+
+        System.out.print("Enter a username: ");
+        String username;
+        L1:do{
+            username = scan.nextLine();
+            if(usernames.contains(username)){
+                System.out.println("Username is already taken.");
+                System.out.print("Enter a username: ");
+            } else {
+                usernames.add(username);
+                break L1;
+                //clearConsole();
+            }
+        }while (true);
+
+        UserProfile user = new UserProfile(name,username);
     }
+
+
+
+
+}
 }
