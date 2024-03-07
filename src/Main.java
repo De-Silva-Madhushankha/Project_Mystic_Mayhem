aeabf5c501e81ffab76a26cb941f8014ad21ae21
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2024-03-07 23:29:45 +0530

    Changed addHero, sellHero, buyHero, updateStatus Added new method called trainHero to create a Character object

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,16 @@ import Equipment.*;
 
 public class Main {
     public static void main(String[] args) {
-        UserProfile user01 = new UserProfile("Razor","Razor_0.1");
-        user01.setHomeground("Arcane");
-
-        Character shooter = new Shooter();
-        Character ranger = new Ranger();
-
-        System.out.println(shooter.getClass().getSuperclass().getSimpleName());
-
-        Equipment fleece = new Fleece();
-        System.out.println(fleece.getClass().getSuperclass().getInterfaces()[0].getSimpleName());
+//        UserProfile user01 = new UserProfile("Razor","Razor_0.1");
+//        user01.setHomeground("Arcane");
+//
+//        Character shooter = new Shooter();
+//        Character ranger = new Ranger();
+//
+//        System.out.println(shooter.getClass().getSuperclass().getSimpleName());
+//
+//        Equipment fleece = new Fleece();
+//        System.out.println(fleece.getClass().getSuperclass().getInterfaces()[0].getSimpleName());
 
     }
 }
--- src/UserProfile.java
+++ src/UserProfile.java
@@ -10,24 +10,17 @@ public class UserProfile {
     private int goldCoins;
     private int XP ;
     private static int  userCount ;
-    private static Set<String> userNames = new LinkedHashSet<String>();
-    private List<Character> army;
-    private Set<String> armyType;
+    private List<Character> army = new arrayList();
+    private Set<String> armyTypes = new HashSet<>();
     private String homeground;
+
 
     UserProfile(String name, String userName){
         this.name = name;
         this.goldCoins = 500;
         this.XP = 0;
-
-        if(userNames.contains(userName)){
-            System.out.println("Username is already taken. Please choose a different username. ");
-        }
-        else{
-            this.userName = userName;
-            this.userID = createUserID();
-            userNames.add(userName);
-        }
+        this.userName = userName;
+        this.userID = createUserID();
         userCount++;
     }
     private int createUserID(){
@@ -44,20 +37,107 @@ public class UserProfile {
         return goldCoins;
     }
 
-    public void setArmy(List<Character> army) {
-        this.armyType = new HashSet<>();
-        for (Character c : army) {
-            Class<?> unitClass = c.getClass();
-            if (!armyType.contains(unitClass)) {
-                if(this.goldCoins >= c.getPrice()){
-                    this.army.add(c);
-                    armyType.add(unitClass);
-                }
-                else System.out.println("Not enough Gold Coins");
+    private Character trainHero(String heroName){
+        Character hero = null;
+        switch(heroName){
 
-            } else {
-                System.out.println("An instance of " + unitClass.getName() + " has already been added. Please Choose a different character from each category");
+            case "Ranger":
+                hero = new Ranger();
+                break;
+            case "Saggitarius":
+                hero = new Saggitarius();
+                break;
+            case "Shooter":
+                hero = new Shooter();
+                break;
+            case "Sunfire":
+                hero = new Sunfire();
+                break;
+            case "Zing":
+                hero = new Zing();
+                break;
+            case "Alchemist":
+                hero = new Alchemist();
+                break;
+            case "Lightbringer":
+                hero = new Lightbringer();
+                break;
+            case "Medic":
+                hero = new Medic();
+                break;
+            case "Saint":
+                hero = new Saint();
+                break;
+            case "Soother":
+                hero = new Soother();
+                break;
+            case "Cavalier":
+                hero = new Cavalier();
+                break;
+            case "Squire":
+                hero = new Squire();
+                break;
+            case "Swiftblade":
+                hero = new Swiftblade();
+                break;
+            case "Templar":
+                hero = new Templar();
+                break;
+            case "Zoro":
+                hero = new Zoro();
+                break;
+            case "Conjurer":
+                hero = new Conjurer();
+                break;
+            case "Eldritch":
+                hero = new Eldritch();
+                break;
+            case "Enchanter":
+                hero = new Enchanter();
+                break;
+            case "Illusionist":
+                hero = new Illusionist();
+                break;
+            case "Warlock":
+                hero = new Warlock();
+                break;
+            case "Basilisk":
+                hero = new Basilisk();
+                break;
+            case "Dragon":
+                hero = new Dragon();
+                break;
+            case "Hydra":
+                hero = new Hydra();
+                break;
+            case "Pegasus":
+                hero = new Pegasus();
+                break;
+            case "Phoenix":
+                hero = new Phoenix();
+                break;
+        }
+
+        return hero;
+
+    }
+
+    public int addHero(String characterName) {
+
+        Character hero = trainHero(characterName);
+        String heroType = hero.getClass().getSuperClass().getSimpleClass().getSimpleName();
+        assert hero != null;
+        if( this.getGoldCoins() >= hero.getPrice()){
+            if(armyTypes.contains(heroType)){
+                return 0;
+            }else{
+                this.goldCoins -= hero.getPrice();
+                army.add(hero);
+                armyTypes.add(heroType);
+                return 1;
             }
+        }else {
+            return -1;
         }
     }
 
@@ -65,57 +145,69 @@ public class UserProfile {
         this.homeground = homeground;
     }
 
-    public void updateStatus(UserProfile opponent, boolean win) {
+    public void updateStatus(UserProfile opponent, String battleOutcome) {
         int opGoldCoins = opponent.getGoldCoins();
-        if(win){
-            this.goldCoins += opGoldCoins * 0.1;
-        }
-        else {
-            this.goldCoins -= this.goldCoins * 0.1;
+
+        switch(battleOutcome){
+            case "victory":
+                this.goldCoins += opGoldCoins * 0.1;
+                break;
+            case "defeat":
+                this.goldCoins -= this.goldCoins * 0.1;
+                break;
+            case "draw":
+                break;
         }
     }
 
-    public void combat(UserProfile opponent) {
-        // Combat logic here
-    }
 
-    public void buyCharacter(Character newCharacter) {
-        if( this.goldCoins >= newCharacter.getPrice()){
-            if (!armyType.contains(newCharacter.getClass())){
-                army.add(newCharacter);
-                armyType.add(newCharacter.getClass());
-                this.goldCoins -= newCharacter.getPrice();
+    public void buyCharacter(String characterName) {
+        Scanner scanner = new Scanner(System.in);
+        if(army.size() == 5){
+            System.out.println("Your Army is already full!");
+            System.out.print("Do You wish to sell a hero(Y/N)? ");
+            String wannaSell = scanner.nextLine();
+
+            if(wannaSell.compareToIgnoreCase("y")){
+                System.out.println(String.format("Your Army :- %s %s %s %s %s",army[0],army[1],army[2],army[3],army[4]));
+                System.out.print("Select a hero to sell : ");
+                String heroToBeSell = scanner.nextLine();
+                Character  sellHero = trainHero(sellHero);
+                sellCharacter(sellHero);
             }
+
             else{
-                System.out.println("You already have a character from that category. Please choose a character from a different category.");
+                System.out.println("Your Army is already full!");
             }
         }
-        else{
-            System.out.println("Not enough gold coins to buy the character");
-        }
+        else
+            addHero(characterName);
     }
 
-    public void sellCharacter(Character oldCharacter) {
+    public void sellCharacter(String characterName) {
+        Character oldCharacter = trainHero(characterName);
         this.goldCoins += (oldCharacter.getPrice() * 0.9);
-        armyClasses.remove(oldCharacter.getClass());
-        army.remove(oldCharacter);
+        String characterType = oldCharacter.getClass().getSuperClass().getSimpleClass().getSimpleName();
+        System.out.println(characterType + "was sold.");
+        armyTypes.remove(characterType)
+        army.remove(characterName);
 
     }
 
     public void buyEquipment(Equipment newEquipment,Character character){
     }
 
-    public void printUserDetails(){
-        String details = String.format("Name: %s\n" +
-                                       "Username: %s\n" +
-                                       "XP: %d\n" +
-                                       "Gold coins: %d\n" +
-                                       "Homeground: %d\n" +
-                                       "Archer: %s\n" +
-                                       "Knight: %S\n" +
-                                       "Mage: %s\n" +
-                                       "Healer: %s\n" +
-                                       "Mythical Creature: %s\n");
+//    public void printUserDetails(){
+//        String details = String.format("Name: %s\n" +
+//                                       "Username: %s\n" +
+//                                       "XP: %d\n" +
+//                                       "Gold coins: %d\n" +
+//                                       "Homeground: %d\n" +
+//                                       "Archer: %s\n" +
+//                                       "Knight: %S\n" +
+//                                       "Mage: %s\n" +
+//                                       "Healer: %s\n" +
+//                                       "Mythical Creature: %s\n");
 
 
 //    @Override
@@ -123,3 +215,7 @@ public class UserProfile {
 //        return "User [Name:"+name+" ,Username:"+userName+" ,XP:"+XP+" ,Gold Coin:"+goldCoins+" ,Home ground:"+homeground+" ]";
 //    }
 }
+
+//    public void combat(UserProfile opponent) {
+//        // Combat logic here
+//    }
